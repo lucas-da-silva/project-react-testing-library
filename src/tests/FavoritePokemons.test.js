1739648ac03036e7c41af899677e6e2a588d274c
lucas-da-silva <lucassouza00012345@gmail.com>
2022-08-16 16:38:52 -0300

    req-3 done

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Tests for page FavoritePokemons', () => {
+  it('the message No favorite pokemon found is displayed on the screen', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFoundText = screen.getByText('No favorite pokemon found');
+    expect(notFoundText).toBeInTheDocument();
+  });
+});
